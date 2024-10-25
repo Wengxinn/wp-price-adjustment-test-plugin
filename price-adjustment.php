f8cdffc720e02bf991eac1ca8a46f9cc3a51e42f
Weng Xinn Chow <z5346077@ad.unsw.edu.au>
2024-10-26 00:08:54 +1100

    Initialised repo and added main plugin file

---
+++ price-adjustment.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Plugin Name: Price Adjustment
+* Plugin URI: http://eux-test-price-adjustment-plugin.com
+* Description: A plugin to adjust metal spot prices for WooCommerce.
+* Version: 1.0
+* Author: Weng Xinn Chow
+* Author URI: https://www.linkedin.com/in/weng-xinn-chow/
+*/
+
+
+
+?>
